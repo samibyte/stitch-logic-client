2ace60dffb89977d0de421ce1c5e408692a7d6fc
samibyte <samibyte.web@gmail.com>
2025-12-22 01:17:17 +0600

    feat: update logo dimensions

--- src/components/ui/Logo.tsx
+++ src/components/ui/Logo.tsx
@@ -1,7 +1,7 @@
 import stitchLogo from "/stitchLogo.png";
-const Logo = ({ width = 60, height = 30 }) => {
+const Logo = () => {
   return (
-    <div className={`relative h-${height} w-${width} border-2`}>
+    <div className={`relative h-30 w-60 border-2`}>
       <img className="w-40 border-2" src={stitchLogo}></img>
       <p className="absolute right-0 text-5xl font-black text-[#090909]/90">
         Logic
